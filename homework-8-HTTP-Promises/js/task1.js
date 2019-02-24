fa152e49ed7df656079dafc80425d5e0b5c8c672
Vetelina Ekaterina <eovetelina@gmail.com>
2019-02-24 22:29:52 +0300

    Add files via upload

---
+++ homework-8-HTTP-Promises/js/task1.js
@@ -0,0 +1,78 @@
+function sendRequest(filePath) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open("GET", filePath);
+    request.setRequestHeader("Content-Type", "application/json; charset=utf8");
+    request.send();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        resolve(request.response);
+      } else if (request.status !== 200) {
+        reject(request.status + " " + request.statusText);
+      }
+    });
+  });
+}
+
+
+let firstRequest, secondRequest;
+
+
+function unique(arr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+    obj[str] = true;
+  }
+  return Object.keys(obj);
+}
+
+
+function bothPromises() {
+  if (typeof firstRequest !== "undefined" && typeof secondRequest !== "undefined") {
+    Promise.all([firstRequest, secondRequest]).then(values => {
+      let arr = [];
+      for (let i = 0; i < values.length; i++) {
+        if (values[i][0] === "{")
+          arr = arr.concat(JSON.parse(values[i]).hobby.split(","));
+      }
+      document.getElementById("hobbies").innerHTML = "The list of hobbies: " + unique(arr);
+    });
+  }
+}
+
+document.getElementById("firstButton").addEventListener("click", () => {
+  firstRequest = sendRequest("../files/first.json")
+    .then(
+      result => {
+        document.getElementById("firstText").innerHTML = result;
+        return result;
+      },
+      error => {
+        document.getElementById("firstText").innerHTML = error;
+        return error;
+      }
+    )
+    .then(data => {
+      bothPromises();
+      return data;
+    });
+});
+
+document.getElementById("secondButton").addEventListener("click", () => {
+  secondRequest = sendRequest("../files/second.json")
+    .then(
+      result => {
+        document.getElementById("secondText").innerHTML = result;
+        return result;
+      },
+      error => {
+        document.getElementById("secondText").innerHTML = error;
+        return error;
+      }
+    )
+    .then(data => {
+      bothPromises();
+      return data;
+    });
+});
